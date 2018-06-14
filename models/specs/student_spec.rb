3f5d6520bac4ca99a79108112e2c4dbf96d5287c
Gary Muir <garymuir17@gmail.com>
2018-06-14 12:14:27 +0100

    first name test for student passed

---
+++ models/specs/student_spec.rb
@@ -0,0 +1,22 @@
+require("minitest/autorun")
+require_relative("../student.rb")
+
+class StudentTest < MiniTest::Test
+
+  def setup()
+    options = {
+      "id" => 1,
+      "first_name" => "Harry",
+      "last_name" => "Potter",
+      "house" => "Gryffindor",
+      "age" => 12
+    }
+    @student = Student.new(options)
+  end
+
+  def test_first_name()
+    result = @student.first_name()
+    assert_equal("Harry", result)
+  end
+
+end
